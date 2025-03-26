9a88dc274b3a602734972b3e0e47115c3c1af567
Dylan MacLeod <dillpicklemac@icloud.com>
2025-03-25 23:36:59 -0400

    added CSV upload, also checking for existing items on creation

--- server/routers/products.py
+++ server/routers/products.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from models.catalog.productModel import productItem
 from datetime import datetime
 from database.database import *
+from fastapi import UploadFile
 
 router = APIRouter()
 productDatabase = database("database/productData.json")
@@ -20,8 +21,11 @@ async def create_product(product: productItem):
     data.update({"time":str(datetime.now())})
     data.update({"id":productDatabase.getDataSize() + 1})
 
-    productDatabase.addData(data)
-    return data
+    success = productDatabase.addData(data)
+    if not success:
+        return {"ok": False, "message":"Product already exists"}
+
+    return {"data": data, "ok": True}
 
 @router.delete("/products/{id}")
 async def delete_product(id: int):
@@ -52,3 +56,30 @@ async def update_Sort(sort: dict):
         return productDatabase.sortData(sort_type)
     else:
         return {"message": "Invalid sort type"}
+
+@router.post("/products/upload")
+async def upload_csv(file: UploadFile):
+    # check file type
+    if not file.content_type == "text/csv":
+        return {"message":"Invalid file type", "ok": False}
+    
+    # read file
+    contents = await file.read()
+    contents = contents.decode("utf-8")
+    contents = contents.split("\n")
+
+    # get headers and data
+    headers = contents[0].split(",")
+    contents = contents[1:]
+
+    # add data to database
+    for content in contents:
+        content = [item.strip() for item in content.split(",")]
+        data = dict(zip(headers, content))
+        data.update({"time":str(datetime.now())})
+        data.update({"id":productDatabase.getDataSize() + 1})
+        productDatabase.addData(data)
+
+    # return the new items
+    return {"ok": True}
+    
