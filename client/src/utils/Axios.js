cf05825f1245b0777680a9bd53056a4eebf02675
Dylan MacLeod <dillpicklemac@icloud.com>
2025-04-02 15:33:14 -0400

    fix for server communication

--- client/src/utils/Axios.js
+++ client/src/utils/Axios.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 
+const ROOT_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
 
   
 // Axios request function, takes in an object with url, method, and data
 function AxiosRequest({ url, method = "get", data = {} }) {
     return axios({
       method: method,
-      url: `http://localhost:8000${url}`,
+      url: `${ROOT_URL}${url}`,
       data: data,
     });
   }
